f694b79e088c1706a31387c137962662bc61e527
kozyrev42 <alexander.kozyrev@pluscards.ru>
2023-03-07 23:58:10 +0700

    тестовый коммит

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
     @foreach($posts as $post)
+        {{-- формирование ссылки вида: /posts/{id} --}}
         <a href="{{ route('posts.show', $post->id) }}">{{$post->title}}</a>
         <hr>
     @endforeach
